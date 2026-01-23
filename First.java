58c4deca16eb07b0575cdf6b4b8ed76ea95852c3
Garbage245 <mbsagoii@gmail.com>
2026-01-23 12:38:12 -0500

    Add files via upload

---
+++ First.java
@@ -0,0 +1,11 @@
+// Myron Sago
+// 1/19/26
+
+public class First{
+    public static void main(String[] args) {
+        System.out.println("Hola mundo :3");
+        System.out.println("Job Title: Computer Network Architect");
+        System.out.println("Why: I like designing digital logic circuits and connecting things together.");
+        System.out.println("Median Salary: $130,390");
+    }
+}
