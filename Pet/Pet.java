1dc46820ba880604e4e85a473fa2c66ba66955d3
Garbage245 <mbsagoii@gmail.com>
2026-02-14 14:34:01 -0500

    Update Pet.java

--- Pet/Pet.java
+++ Pet/Pet.java
@@ -1,48 +1,58 @@
 // Myron Sago II
 // 2/2/2026
+
 public class Pet{
     // Pet name, type, and age
     private String petName;
     private String petType;
     private int petAge;
 
+    // Default pet information if called with no arguments
     public Pet(){
         petName = "Animal";
         petType = "Dog";
-        petAge = 1; 
+        petAge = 1;
     }
+
+    // Sets pet private variables to user inputs
     public Pet(String name, String type, int age){
-    // Sets pet name to an empty string
         setName(name);
         setType(type);
         setAge(age);
 
     }
-    // Returns name variable
+
+    // Returns petName
     public String getName(){
         return petName;
     }
+
     // Sets petName as user input
     public void setName(String input){
         petName = input;
     }
-    // Sets petName as user input
+
+     // Returns petType
     public String getType(){
         return petType;
     }
-    // Sets petName as user input
+
+    // Sets petType as user input
     public void setType(String input){
         petType = input;
     }
+    // Returns petAge
     public int getAge(){
         return petAge;
     }
+
+    // Sets petAge as user input
     public void setAge(int input){
         petAge = input;
     }
-    
-    public String ToString(){
+
     // Creates a String with pet information
+    public String ToString(){
         String output = "Pet Information:";
         output += "\nType: ";
         output += getType();
