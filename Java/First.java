f26256092ca8370eab77c212f499d510a5fd1d5b
Garbage245 <mbsagoii@gmail.com>
2026-04-13 13:51:41 -0400

    organization

---
+++ Java/First.java
@@ -0,0 +1,12 @@
+// Myron Sago
+// 1/19/26
+
+public class First{
+    public static void main() {
+        System.out.println("Hola mundo :3");
+        System.out.println("Job Title: Computer Network Architect");
+        System.out.println("Why: I like designing digital logic circuits and connecting things together.");
+        System.out.println("Median Salary: $130,390");
+    }
+}
+// Test
